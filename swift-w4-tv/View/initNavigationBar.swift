d4759f915424d1cd36e7272f2814162a8f4521e3
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-25 17:03:25 +0900

    navigation bar 추가

---
+++ swift-w4-tv/View/initNavigationBar.swift
@@ -0,0 +1,34 @@
+//
+//  navigationBar.swift
+//  swift-w4-tv
+//
+//  Created by 이준형 on 2021/01/25.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class naviBar {
+    
+    var rightButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.image = UIImage(systemName: "heart.fill")
+        button.style = .plain
+        return button
+    }()
+    
+    func initNavigationBar(view : UIViewController){
+        view.navigationController?.navigationBar.isHidden = false
+        view.navigationController?.navigationBar.backgroundColor = .white
+        rightButton.target = self
+        rightButton.action = #selector(buttonPressed)
+        view.navigationController?.navigationBar.topItem?.title = "emoticon"
+        view.navigationController?.navigationBar.topItem?.rightBarButtonItem = rightButton
+    }
+    
+    @objc func buttonPressed(){
+        print("like!")
+    }
+
+}
