db042bea74327c91c8e95844d86f5a305e32daeb
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-25 23:12:59 +0900

    scrollview 생성

--- swift-w4-tv/View/OriginalProgramScrollView.swift
+++ swift-w4-tv/View/OriginalProgramScrollView.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class OriginalProgramScrollView {
+    private let screenWidth = UIScreen.main.bounds.size.width
+    private let screenHeight = UIScreen.main.bounds.size.height
+    
+    private var programScrollView : UIScrollView = UIScrollView()
+    
+    func initOriginalProgramScrollView(view : UIViewController){
+        programScrollView.frame = CGRect(x: 0, y: screenHeight / 3, width: screenWidth, height: screenHeight*2 / 3 )
+        programScrollView.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.1)
+        view.view.addSubview(programScrollView)
+    }
+}
