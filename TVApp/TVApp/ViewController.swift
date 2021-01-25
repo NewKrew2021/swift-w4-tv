777c288768b94dababbecc4ab3a0211d6a230fb2
Herb Salt <herb.salt@kakaocorp.com>
2021-01-25 10:37:48 +0900

    make project

---
+++ TVApp/TVApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVApp
+//
+//  Created by herb.salt on 2021/01/25.
+//  Copyright © 2021 com.kakaocorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
