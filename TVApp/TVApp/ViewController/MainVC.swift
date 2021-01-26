6d2dad112b9ea7ad411838dacff4972612b8a045
Herb Salt <herb.salt@kakaocorp.com>
2021-01-26 11:32:21 +0900

    make MainVC(+StoryBoard)

---
+++ TVApp/TVApp/ViewController/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  TVApp
+//
+//  Created by herb.salt on 2021/01/26.
+//  Copyright © 2021 com.kakaocorp. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
