4cdc780e90512aa77fc19361b55b5bcd18acf34a
blue.1 <blue.1@kakaocorp.com>
2021-01-26 10:46:36 +0900

    collectionView 추가

---
+++ TVApp/TVApp/View/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  TVApp
+//
+//  Created by 이청원 on 2021/01/26.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
--- TVApp/TVApp/ViewController/MainViewController.swift
+++ TVApp/TVApp/ViewController/MainViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
 //    func jsonParsing() {
@@ -40,3 +43,15 @@ class MainViewController: UIViewController {
 //    }
 }
 
+extension MainViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
