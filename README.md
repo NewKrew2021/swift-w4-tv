50d6cd1c77bd4dde21902c6e500c6db0cdb5a3b3
Herb Salt <herb.salt@kakaocorp.com>
2021-01-26 18:50:10 +0900

    make SampleData, CollectionView

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # swift-w4-tv
 모바일 4주차 TV 저장소
+
+
+1. make MainVC
+- MainViewController 생성
+
+
+2. make Model
+- OriginalTV, LiveTV, Channel, Clip, Live 모델 생성
+
+
+3. make sample data, CollectionView
+- Json 파싱을 통한 sample data 생성
+- sample data를 이용한 CollectionView 생성
+![Simulator Screen Shot - iPhone 11 - 2021-01-26 at 18 41 24](https://user-images.githubusercontent.com/28801805/105828963-1c412100-6007-11eb-9a66-73566e156b1b.png)
+![Simulator Screen Shot - iPad (7th generation) - 2021-01-26 at 18 41 14](https://user-images.githubusercontent.com/28801805/105828967-1d724e00-6007-11eb-9e00-cd435a1a8f2b.png)
--- TVApp/TVApp/ViewController/MainVC.swift
+++ TVApp/TVApp/ViewController/MainVC.swift
@@ -10,21 +10,92 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var mainSegmentControl: UISegmentedControl!
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
+    let sampleData = SampleData()
+    var currentSegment = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        initView()
     }
     
+    func initView() {
+        
+        mainCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mainCollectionView.delegate = self
+        mainCollectionView.dataSource = self
+        
+        var width = UIScreen.main.bounds.size.width
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            //item 높이: 사진의 높이 + 텍스트 높이 + view 간격 합
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute((width - 30) * 37/62 + 50 + 30))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
-    /*
-    // MARK: - Navigation
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: itemSize.heightDimension)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+
+        }
+        mainCollectionView.collectionViewLayout = layout
+        
     }
-    */
-
+    
+    @IBAction func mainSegmentChanged(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            currentSegment = 0
+        case 1:
+            currentSegment = 1
+        default:
+            return
+        }
+        mainCollectionView.reloadData()
+        
+    }
 }
+
+extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if(currentSegment == 0) {
+            return sampleData.originalTVs.count
+        }
+        else {
+            return sampleData.liveTVs.count
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as! MainCollectionViewCell
+        
+        if(currentSegment == 0) {
+            cell.thumnbnailImageView.image = UIImage(named: (sampleData.originalTVs[indexPath.row].clip?.thumbnailUrl!)!)
+            cell.titleLabel.text = sampleData.originalTVs[indexPath.row].clip?.title
+            cell.thumbnailLabel.text = String((sampleData.originalTVs[indexPath.row].clip?.duration)! as Int)
+            cell.channelLabel.text = sampleData.originalTVs[indexPath.row].channel?.name
+            cell.visitCountLabel.text = "▶︎"+String((sampleData.originalTVs[indexPath.row].channel?.visitCount)! as Int)
+            cell.createTimeLabel.text = sampleData.originalTVs[indexPath.row].createTime
+        }
+        else {
+            cell.thumnbnailImageView.image = UIImage(named: (sampleData.liveTVs[indexPath.row].live?.thumbnailUrl!)!)
+            cell.titleLabel.text = sampleData.liveTVs[indexPath.row].live?.title
+            cell.thumbnailLabel.text = String((sampleData.liveTVs[indexPath.row].live?.duration)! as Int)
+            cell.channelLabel.text = sampleData.liveTVs[indexPath.row].channel?.name
+            cell.visitCountLabel.text = "▶︎"+String((sampleData.liveTVs[indexPath.row].channel?.visitCount)! as Int)
+            cell.createTimeLabel.text = sampleData.liveTVs[indexPath.row].createTime
+        }
+        return cell
+    
+    }
+    
+}
+
