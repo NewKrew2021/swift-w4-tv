82c35bfda7bf4882c9035b3fdaa28803570a9d85
blue.1 <blue.1@kakaocorp.com>
2021-01-26 17:40:31 +0900

    json 파싱해서 collection view에 데이터 띄우기

--- TVApp/TVApp/View/CollectionViewCell.swift
+++ TVApp/TVApp/View/CollectionViewCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    func setData(original: Original) {
+        thumbnailImageView.image = UIImage(named: original.clip?.thumbnailUrl ?? "")
+        titleLabel.text = original.clip?.title
+    }
 }
--- TVApp/TVApp/ViewController/MainViewController.swift
+++ TVApp/TVApp/ViewController/MainViewController.swift
@@ -9,49 +9,54 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    enum VideoType {
+        case original, live
+    }
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    private var originals : [Original] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        decodeData(type: .original)
     }
+    
+    func decodeData(type : VideoType) {
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let originalDataAsset: NSDataAsset = NSDataAsset.init(name: "original") else {return}
+        guard let liveDataAsset: NSDataAsset = NSDataAsset.init(name: "live") else {return}
 
-//    func jsonParsing() {
-//        var originals : [Original] = []
-//        let jsonDecoder: JSONDecoder = JSONDecoder()
-//        guard let dataAsset: NSDataAsset = NSDataAsset.init(name: "live") else {return}
-//
-//        do {
-//            originals = try jsonDecoder.decode([Original].self, from: dataAsset.data)
-//            print(originals)
-//        } catch let DecodingError.dataCorrupted(context) {
-//            print(context)
-//        } catch let DecodingError.keyNotFound(key, context) {
-//            print("Key '\(key)' not found:", context.debugDescription)
-//            print("codingPath:", context.codingPath)
-//        } catch let DecodingError.valueNotFound(value, context) {
-//            print("Value '\(value)' not found:", context.debugDescription)
-//            print("codingPath:", context.codingPath)
-//        } catch let DecodingError.typeMismatch(type, context)  {
-//            print("Type '\(type)' mismatch:", context.debugDescription)
-//            print("codingPath:", context.codingPath)
-//        } catch {
-//            print("error: ", error)
-//        }
-//    }
+        do {
+            if type == .original {
+                originals =  try jsonDecoder.decode([Original].self, from: originalDataAsset.data)
+            } else if type == .live {
+                originals = try jsonDecoder.decode([Original].self, from: liveDataAsset.data)
+            }
+            collectionView.reloadData()
+        } catch let error {
+            print("error: ", error)
+        }
+    }
+    
 }
 
-extension MainViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+extension MainViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return self.originals.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.setData(original: originals[indexPath.row])
         return cell
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 208 166
+        let width = self.collectionView.frame.size.width
+        return CGSize(width: width, height: width * 0.79)
+    }
 }
