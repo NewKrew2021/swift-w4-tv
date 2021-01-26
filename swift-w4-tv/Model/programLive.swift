7d6466af866988e1e8c1ed82211d224e129c48a6
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-26 18:06:25 +0900

    program data struct

--- swift-w4-tv/Model/programLive.swift
+++ swift-w4-tv/Model/programLive.swift
@@ -2,8 +2,94 @@
 //  programLive.swift
 //  swift-w4-tv
 //
-//  Created by 이준형 on 2021/01/26.
+//  Created by 이준형 on 2021/01/25.
 //  Copyright © 2021 이준형. All rights reserved.
 //
 
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let programLive = try? newJSONDecoder().decode(ProgramLive.self, from: jsonData)
+
 import Foundation
+
+// MARK: - ProgramLiveElement
+struct ProgramLiveElement: Codable {
+    let id, channelID, liveID: Int
+    let displayTitle, createTime: String
+    let channel: Channel
+    let live: Live
+    let videoType: VideoType
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case channelID = "channelId"
+        case liveID = "liveId"
+        case displayTitle, createTime, channel, live, videoType
+    }
+}
+
+// MARK: - Live
+struct Live: Codable {
+    let id, userID, channelID, categoryID: Int
+    let liveType: LiveType
+    let title, liveDescription: String
+    let status: Status
+    let isOpen, canScrap, canLink, shouldGeoBlock: Bool
+    let ageLimit: AgeLimit
+    let startTime: String
+    let reportCount: Int
+    let createTime: String
+    let playCount, likeCount: Int
+    let service: Service
+    let thumbnailURL, ccuCount: String
+    let duration: Int
+    let isShowCcuCount: Bool
+    let liveGeoBlock: LiveGeoBlock?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID = "userId"
+        case channelID = "channelId"
+        case categoryID = "categoryId"
+        case liveType, title
+        case liveDescription = "description"
+        case status, isOpen, canScrap, canLink, shouldGeoBlock, ageLimit, startTime, reportCount, createTime, playCount, likeCount, service
+        case thumbnailURL = "thumbnailUrl"
+        case ccuCount, duration, isShowCcuCount, liveGeoBlock
+    }
+}
+
+enum AgeLimit: String, Codable {
+    case all = "ALL"
+}
+
+// MARK: - LiveGeoBlock
+struct LiveGeoBlock: Codable {
+    let geoBlockType: String
+    let nations: [String]
+}
+
+enum LiveType: String, Codable {
+    case streaming = "STREAMING"
+}
+
+// MARK: - Service
+struct Service: Codable {
+    let name: Name
+}
+
+enum Name: String, Codable {
+    case kakaoTv = "kakao_tv"
+}
+
+enum Status: String, Codable {
+    case onair = "ONAIR"
+}
+
+enum VideoType: String, Codable {
+    case live = "LIVE"
+}
+
+typealias ProgramLive = [ProgramLiveElement]
--- swift-w4-tv/Model/programOrg.swift
+++ swift-w4-tv/Model/programOrg.swift
@@ -1,27 +1,117 @@
 //
-//  program.swift
+//  programOrg.swift
 //  swift-w4-tv
 //
 //  Created by 이준형 on 2021/01/25.
 //  Copyright © 2021 이준형. All rights reserved.
 //
 
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let programOrg = try? newJSONDecoder().decode(ProgramOrg.self, from: jsonData)
+
 import Foundation
 
-struct programOrg {
-    var clipTitle : String
-    var clipThumnailUrl : String
-    var clipDuration : Int
-    var channelName : String
-    var channelVisitCount : Int
-    var channelCreateTime : String
+// MARK: - ProgramOrgElement
+struct ProgramOrgElement: Codable {
+    let id, channelID, clipID: Int
+    let displayTitle, createTime: String
+    let channel: Channel
+    let clip: Clip
+    let videoType: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case channelID = "channelId"
+        case clipID = "clipId"
+        case displayTitle, createTime, channel, clip, videoType
+    }
 }
 
-struct programLive {
-    var clipTitle : String
-    var clipThumnailUrl : String
-    var livePlayCount : Int
-    var channelName : String
-    var channelVisitCount : Int
-    var channelCreateTime : String
+// MARK: - Channel
+struct Channel: Codable {
+    let id, userID, categoryID: Int
+    let name, channelDescription: String
+    let headPlaylistID, subscriberCount, visitCount: Int
+    let isOpen, shouldFeedSearcher, isLegacy: Bool
+    let createTime: String
+    let canAutoUploadLiveClip, canUploadClip: Bool
+    let clipCount: Int
+    let canLive: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userID = "userId"
+        case categoryID = "categoryId"
+        case name
+        case channelDescription = "description"
+        case headPlaylistID = "headPlaylistId"
+        case subscriberCount, visitCount, isOpen, shouldFeedSearcher, isLegacy, createTime, canAutoUploadLiveClip, canUploadClip, clipCount, canLive
+    }
+}
+
+// MARK: - Clip
+struct Clip: Codable {
+    let id: Int
+    let vid, title, clipDescription: String
+    let userID, channelID, categoryID: Int
+    let status: String
+    let duration: Int
+    let isVertical: Bool
+    let thumbnailURL: String
+    let sourceURL: JSONNull?
+    let ageLimit: String
+    let playCount, likeCount, commentCount: Int
+    let createTime: String
+    let violationType: JSONNull?
+    let clipGeoBlock: ClipGeoBlock
+    let adultThumbnail, wasLive, like: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case id, vid, title
+        case clipDescription = "description"
+        case userID = "userId"
+        case channelID = "channelId"
+        case categoryID = "categoryId"
+        case status, duration, isVertical
+        case thumbnailURL = "thumbnailUrl"
+        case sourceURL = "sourceUrl"
+        case ageLimit, playCount, likeCount, commentCount, createTime, violationType, clipGeoBlock, adultThumbnail, wasLive, like
+    }
+}
+
+// MARK: - ClipGeoBlock
+struct ClipGeoBlock: Codable {
+    let geoBlockType: String
+    let nations: [String]
+}
+
+typealias ProgramOrg = [ProgramOrgElement]
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+//
+//    public required init(from decoder: Decoder) throws {
+//        let container = try decoder.singleValueContainer()
+//        if !container.decodeNil() {
+//            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+//        }
+//    }
+//
+//    public func encode(to encoder: Encoder) throws {
+//        var container = encoder.singleValueContainer()
+//        try container.encodeNil()
+//    }
 }
