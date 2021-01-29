2fc76a9c4127b3ff8f2b00c3d505853ae16d2a4b
blue.1 <blue.1@kakaocorp.com>
2021-01-29 17:11:27 +0900

    스토리보드에서 like table cell에 label 추가

---
+++ TVApp/TVApp/View/LikeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LikeTableViewCell.swift
+//  TVApp
+//
+//  Created by 이청원 on 2021/01/29.
+//
+
+import UIKit
+
+class LikeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
