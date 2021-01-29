1d30dec0d72b2d50aaebf30a0dad5e6ea9a536e0
blue.1 <blue.1@kakaocorp.com>
2021-01-29 16:45:17 +0900

    즐겨찾기 목록에서 user default를 사용하기위한 Like와 LikeData 구조체 구현

---
+++ TVApp/TVApp/Model/Like.swift
@@ -0,0 +1,34 @@
+//
+//  Like.swift
+//  TVApp
+//
+//  Created by 이청원 on 2021/01/29.
+//
+
+import Foundation
+
+struct Likes {
+    private static var data = LikeData.getData() ?? [Int : Like]()
+    
+    static func addOrRemoveLike(id : Int, like : Like) {
+        if data[id] == nil {
+            data[id] = like
+        } else {
+            data.removeValue(forKey: id)
+        }
+        LikeData.updateData(data)
+    }
+    
+    static func getKeys() -> Dictionary<Int, Like>.Keys {
+        return data.keys
+    }
+    
+    static func getItem(key : Int) -> Like? {
+        return data[key]
+    }
+}
+
+struct Like: Codable {
+    let title: String
+    let channelName: String
+}
