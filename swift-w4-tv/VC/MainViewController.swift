8237b8da444a7a6f75cc99f1d7585a11f0f89cd3
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-29 11:40:32 +0900

    즐겨찾기 뷰 전환 및 제스처 인식기 삭제

--- swift-w4-tv/VC/MainViewController.swift
+++ swift-w4-tv/VC/MainViewController.swift
@@ -27,17 +27,15 @@ class MainViewController: UIViewController {
     private var cellheight = CGFloat()
     
     private var flag = true
-    private var favoritePrograms = favorite()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initMainViewController()
-        setLongPressGestureRecognizer()
         NotificationCenter.default.addObserver(self, selector: #selector(saveFunc(_:)), name: NSNotification.Name("saveData"), object: nil)
     }
     
     @objc func saveFunc(_ notification: Notification){
-        favoritePrograms.saveUserDefault()
+        favorite.saveUserDefault()
     }
     
     func initMainViewController(){
@@ -83,7 +81,9 @@ class MainViewController: UIViewController {
     }
     
     @objc func buttonPressed(){
-        
+        let favoriteVC = FavoriteTableViewController()
+        present(favoriteVC, animated: true, completion: nil)
+        favorite.saveUserDefault()
     }
     
     func reloadData(){
@@ -148,38 +148,6 @@ extension MainViewController : UICollectionViewDataSource, UICollectionViewDeleg
             cellwidth = 0
         }
         cellheight = cellwidth * 0.8
-    }
-}
-
-extension MainViewController {
-    func setLongPressGestureRecognizer(){
-        let LongPress: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.LongPressView(_:)))
-        LongPress.minimumPressDuration = 2
-        self.view.addGestureRecognizer(LongPress)
-    }
-    
-    @objc func LongPressView(_ sender: UILongPressGestureRecognizer){
-        if let indexPath = self.myCollectionView?.indexPathForItem(at: sender.location(in: self.myCollectionView)) {
-            let cell = self.myCollectionView?.cellForItem(at: indexPath)
-            
-            if  (sender.state != UIGestureRecognizer.State.ended && sender.state != UIGestureRecognizer.State.cancelled && sender.state != UIGestureRecognizer.State.failed
-                && sender.state != UIGestureRecognizer.State.changed) {
-                let title : String
-                let name : String
-                let id : Int
-                switch type {
-                case .Original:
-                    title = jsonData.originalPrograms[indexPath[1]].clip.title
-                    name = jsonData.originalPrograms[indexPath[1]].channel.name
-                    id = jsonData.originalPrograms[indexPath[1]].clip.id
-                default:
-                    title = jsonData.livePrograms[indexPath[1]].live.title
-                    name = jsonData.livePrograms[indexPath[1]].channel.name
-                    id = jsonData.livePrograms[indexPath[1]].live.id
-                }
-                favoritePrograms.addOrRemoveProgram(title: title, channelName: name, id: id)
-            }
-        }
     }
 }
 
---
+++ swift-w4-tv/View/ProgramCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgramCollectionView.swift
+//  swift-w4-tv
+//
+//  Created by 이준형 on 2021/01/29.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import UIKit
+
+class ProgramCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
